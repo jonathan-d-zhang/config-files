0c842004c38183ea0c24a120a3fa5b99ffb57656
jonathan-d-zhang <69145546+jonathan-d-zhang@users.noreply.github.com>
2022-09-29 11:48:38 -0400

    Set RIPGREP_CONFIG_PATH so ripgrep can find config

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
-set -gx FZF_DEFAULT_COMMAND 'rg --files --sortr path'
+set -x RIPGREP_CONFIG_PATH "$HOME/.config/.ripgreprc"
+set -x FZF_DEFAULT_COMMAND 'rg --files'
 status is-interactive; and pyenv init --path | source
 pyenv init - | source
 starship init fish | source
