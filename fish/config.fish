22f9136f8eb6a99f91d18f21b47756d2dc3684fd
SavagePastaMan <69145546+SavagePastaMan@users.noreply.github.com>
2022-10-03 18:16:45 -0400

    Alias clip to clip.exe

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,10 @@ set -x FZF_DEFAULT_COMMAND 'rg --files'
 status is-interactive; and pyenv init --path | source
 pyenv init - | source
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/jonathan/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
+# Created by `pipx` on 2022-09-29 19:53:45
+set PATH $PATH /home/jonathan/.local/bin
